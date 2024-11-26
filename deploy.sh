90b481ba6157c4abfe1ed2fd8f9f7a7511886d65
unknown <abdoulayeilliassou9753@gmail.com>
2024-11-27 00:28:54 +0800

    Added deployment script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Update system packages
+sudo apt-get update
+sudo apt-get install -y python3-pip git
+
+# Clone the repository
+git clone https://github.com/abdoula666/virsualsearch.git
+cd virsualsearch
+
+# Install Python dependencies
+pip3 install -r requirements.txt
+
+# Run the application
+python3 app.py
